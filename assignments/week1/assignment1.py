2861c3c0b1c07666cff5cd0488f444bfbcedf1db
MelikaYaribakht <Melika.Yaribakht@stud.leuphana.de>
2026-04-20 11:16:20 +0200

    Add week 1 ASCII art assignment

---
+++ assignments/week1/assignment1.py
@@ -0,0 +1,14 @@
+# ASCII Art - Boat on Water
+
+print("            |")
+print("           /|\\")
+print("          /_|_\\")
+print("        ____|____")
+print("        \\       /")
+print("         \\_____/")
+
+print("         ~ sailing ~") 
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("      ~       ~      ~        ~   ")
+print("  ~      ~       ~       ~        ")
