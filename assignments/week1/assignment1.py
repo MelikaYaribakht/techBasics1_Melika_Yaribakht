1886842742d8aac721ddcb4d4d230a1cc9642d9c
MelikaYaribakht <Melika.Yaribakht@stud.leuphana.de>
2026-04-20 11:45:42 +0200

    assignment1.py

--- assignments/week1/assignment1.py
+++ assignments/week1/assignment1.py
@@ -1,14 +1,10 @@
 # ASCII Art - Boat on Water
 
-print("            |")
-print("           /|\\")
-print("          /_|_\\")
-print("        ____|____")
-print("        \\       /")
-print("         \\_____/")
-
-print("         ~ sailing ~") 
-
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("            |\ ")
+print("           /| \\")
+print("          /_|__\\")
+print("        ____|_____")
+print("        \        /")
+print("~~~~~~~~ \______/~~~~~~~~~~~~~~~~~")
 print("      ~       ~      ~        ~   ")
 print("  ~      ~       ~       ~        ")
